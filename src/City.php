820713b6d84baff53cbe846aac07133f95609832
Michael okoh <ordrizzy@gmail.com>
2019-05-11 01:48:34 +0100

    Cities, States and Countries Models

---
+++ src/City.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ichtrojan\Location;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+}
---
+++ src/Country.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ichtrojan\Location;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    //
+}
