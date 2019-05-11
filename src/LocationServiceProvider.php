d3e0138b82e999f909e8c4c8b33280f45cc14baa
Michael okoh <ordrizzy@gmail.com>
2019-05-11 01:49:05 +0100

    The Service Provider

---
+++ src/LocationServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ichtrojan\Location;
+
+use Illuminate\Support\ServiceProvider;
+
+class LocationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('ichtrojan\location\LocationController');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+    }
+}
