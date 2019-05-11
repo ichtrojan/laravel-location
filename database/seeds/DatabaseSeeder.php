d4c14c73d417121359b7a523ab153055fe3f59d4
Michael okoh <ordrizzy@gmail.com>
2019-05-11 01:47:43 +0100

    Cities, States and Countries Seeders

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CountriesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+    }
+}
