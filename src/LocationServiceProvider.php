a49aa0f6c68e15a8bf9c8c3ffa06802fba3c7f4f
ichtrojan <ordrizzy@gmail.com>
2020-01-04 14:57:27 +0100

    Don't publish migrations and seeders | Load Seeder class to boot | cleanup | changed publish tag

--- src/LocationServiceProvider.php
+++ src/LocationServiceProvider.php
@@ -14,8 +14,8 @@ class LocationServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->make('Ichtrojan\Location\Http\Controllers\LocationController');
-
-        $this->mergeConfigFrom(__DIR__ . "/../publishable/config/laravel-location.php", 'laravel-location');
+        $this->app->make('Ichtrojan\Location\Seeds\LocationDatabaseSeeder');
+        $this->mergeConfigFrom(__DIR__ . "/../publishable/config/location.php", 'laravel-location');
     }
 
     /**
@@ -29,43 +29,14 @@ class LocationServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         if ($this->app->runningInConsole()) {
-            $this->publishMigrations();
             $this->publishConfigs();
-            $this->registerPublishableResources();
         }
-    }
-
-    /**
-     * Register the publishable files.
-     */
-    private function registerPublishableResources()
-    {
-        $publishablePath = dirname(__DIR__).'/publishable';
-
-        $publishable = [
-            'laravel-location-seeds' => [
-                "{$publishablePath}/database/seeds/" => database_path('seeds'),
-            ]
-        ];
-
-        foreach ($publishable as $group => $paths) {
-            $this->publishes($paths, $group);
-        }
-    }
-
-    protected function publishMigrations()
-    {
-        $this->publishes([
-            __DIR__ . "/database/migrations/2019_05_11_000000_create_cities_table.php" => database_path('migrations/' . date("Y_m_d_His", time()) . '_create_cities_table.php'),
-            __DIR__ . "/database/migrations/2019_05_11_000000_create_countries_table.php" => database_path('migrations/' . date("Y_m_d_His", time()) . '_create_countries_table.php'),
-            __DIR__ . "/database/migrations/2019_05_11_000000_create_states_table.php" => database_path('migrations/' . date("Y_m_d_His", time()) . '_create_states_table.php')
-        ], 'laravel-location-migrations');
     }
 
     protected function publishConfigs()
     {
         $this->publishes([
-            __DIR__ . "/../publishable/config/laravel-location.php" => config_path('laravel-location.php'),
-        ], 'laravel-location-config');
+            __DIR__ . "/../publishable/config/location.php" => config_path('location.php'),
+        ], 'laravel-location');
     }
 }
